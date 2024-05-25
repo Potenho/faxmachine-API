5ab5bc4d50ffbac717db041a8b33dc51b3bb3e1e
Purple Pote <viisael97@hotmail.com>
2024-05-25 11:02:34 -0300

    Add: Created run.py

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import create_app
+from database.models import db
+
+
+if __name__ == '__main__':
+    app = create_app()
+
+    with app.app_context():
+        db.create_all()
+        
+    app.run(debug=True)
